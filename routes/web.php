0f375258d29c2e096e9dc4ea65a165718e21ee2e
=Rafael <rafael@one-shift.com>
2020-08-11 15:02:24 +0100

    Added web routes registration

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,17 @@
 <?php
 
 // Web routes here
+
+use Core\Router as Router;
+
+Router::get('/', function() {
+  echo "Homepage!";
+});
+
+Router::get('{lg}/', function() {
+  echo "Homepage!";
+});
+
+Router::get('{lg}/{page}/', function($lg, $page) {
+  echo "this is {$page} in {$lg}";
+});
