7b48fac544201ba106161bf429285c73e8ae4e30
=Rafael <rafael@one-shift.com>
2020-08-11 01:30:07 +0100

    Configs testing in the bootstrap file

--- app/init.php
+++ app/init.php
@@ -6,3 +6,19 @@
 --------------------------------------------------------------------------
 |
 */
+
+use Core\Config as Config;
+
+
+$appName = Config('app.name');
+
+Config::alias("db", "database");
+Config::alias("lg", "lang");
+Config::alias("app", "app");
+
+$cfg = Config::build();
+
+echo $cfg->app->name;
+echo '<br>';
+echo '<br>';
+echo $appName;
