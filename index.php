c534c55c7a37009fd1a86a950c5304e9cf8b89fe
rdnine <rafael@rdnine.dev>
2020-08-21 01:48:01 +0100

    New bootstrap system

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Lux - A PHP Micro-Framework for Fast Development
+ * 
+ * @package Lux
+ * @author Rafael Duarte <rafael@rdnine.dev>
+ */
+
+ require_once __DIR__.'/public/index.php';
