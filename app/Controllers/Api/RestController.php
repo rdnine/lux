7e3519048c8af253b233ab68300ea3916cb2f050
rdnine <rafael@rdnine.dev>
2020-08-22 16:01:05 +0100

    Api and Error Handlers Controllers added

---
+++ app/Controllers/Api/RestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use App\Controllers\Controller as Controller;
+
+class RestController extends Controller
+{
+    function __construct()
+    {
+        echo ".::REST CONTROLLER::.";
+    }
+}
---
+++ app/Controllers/Errors/ExceptionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Errors;
+
+use App\Controllers\Controller as Controller;
+
+class ExecptionController extends Controller
+{
+    function __construct()
+    {
+        echo ".::EXCENPTIONS HANDLER::.";
+    }
+}
