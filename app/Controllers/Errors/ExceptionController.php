5c6c1ddd733b6c6211c7a9e01af2eff695960bf4
=Rafael <rafael@one-shift.com>
2020-08-24 15:25:42 +0100

    Controller added for Exceptions Handling

--- app/Controllers/Errors/ExceptionController.php
+++ app/Controllers/Errors/ExceptionController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controllers\Errors;
 
+use Core\View as View;
+use Core\Config as Config;
 use App\Controllers\Controller as Controller;
 
-class ExecptionController extends Controller
+class ExceptionController extends Controller
 {
-    function __construct()
+    private function __construct()
     {
         echo ".::EXCENPTIONS HANDLER::.";
+    }
+
+    public static function notFound($message = "")
+    {
+      View::render(["head" => self::head(), "message" => $message], View::load("errors/404.html"));
+    }
+
+    private static function head()
+    {
+      return View::c2r([
+      "app-title" => "404 - NOT FOUND | " . Config::app('name'),
+      "app-path" => Config::paths('base'),
+      "keywords" => "",
+      "description" => ""
+    ], View::load("components/head.html"));
     }
 }
